3885176258e7480558947fbb92413a9cf603972d
Mihir Yadav <Mihiry4@gmail.com>
2021-12-04 18:38:31 -0700

    creating sql statements and inserting them into sql files for customers and apppointments

---
+++ insertAppointment.sql
@@ -0,0 +1,2 @@
+INSERT INTO appointmentTable VALUES (-1, -1, 2, 0);
+INSERT INTO appointmentTable VALUES (-1, -1, 2, 0);
---
+++ insertCustomers.sql
@@ -0,0 +1,2 @@
+INSERT INTO customerTable VALUES (-1, 'as', 'ki', 2, -1, 2021-12-04, 2033-12-01);
+INSERT INTO customerTable VALUES (-1, 'qwe', 'rty', 2, -1, 2021-12-04, 2033-12-01);
