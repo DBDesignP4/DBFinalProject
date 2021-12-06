f487b38cb31d7314ebaa6718b51ae5a277c9c76d
Mihir Yadav <Mihiry4@gmail.com>
2021-12-06 09:08:27 -0700

    added createAll.sql

---
+++ createAll.sql
@@ -0,0 +1,42 @@
+CREATE TABLE department (
+        dID numeric(1) Primary Key NOT NULL,
+        dName VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE transaction (
+        tID numeric(7) Primary Key NOT NULL,
+        amount INT NOT NULL,
+        DID INT NOT NULL,
+        time DATE NOT NULL,
+        FOREIGN KEY (DID) REFERENCES department(dID)
+);
+
+CREATE TABLE employee (
+        eID numeric(5) Primary Key NOT NULL,
+        fName VARCHAR(25) NOT NULL,
+        lName VARCHAR(25) NOT NULL,
+        DID INT,
+        FOREIGN KEY (DID) REFERENCES department(dID)
+);
+
+CREATE TABLE customer (
+	cID numeric(5) Primary Key NOT NULL,
+	fName VARCHAR(25) NOT NULL,
+	lName VARCHAR(25) NOT NULL,
+	DID INT,
+	TID INT,
+	startDate DATE NOT NULL,
+	endDate DATE NOT NULL,
+	FOREIGN KEY (DID) REFERENCES department(dID),
+	FOREIGN KEY (TID) REFERENCES transaction(tID)
+);
+
+CREATE TABLE appointment (
+	aID INT Primary Key NOT NULL,
+        CID INT,
+        EID INT,
+        status INT DEFAULT 0,
+        FOREIGN KEY (CID) REFERENCES customer(cID),
+        FOREIGN KEY (EID) REFERENCES employee(eID)
+);
+
