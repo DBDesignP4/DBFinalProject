a6f6381fa6f9c1047fb1ded27f843e86955f37eb
rubinyang0401 <rubinyang0401@dhcp-10-142-179-181.uawifi.arizona.edu>
2021-12-06 13:03:23 -0700

    q 123 working

--- Prog4.java
+++ Prog4.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -16,6 +18,28 @@ import java.util.TreeMap;
  * Description: 
  */
 public class Prog4 {
+	
+	public static class Pair implements Comparable{
+		int index;
+		int value;
+		public Pair(int i, int v) {
+			index = i;
+			value = v;
+		}
+
+
+
+
+
+
+		@Override
+		public int compareTo(Object o) {
+			Prog4.Pair temp = (Pair)o;
+			// TODO Auto-generated method stub
+			return Integer.valueOf(value).compareTo(temp.value);
+		}
+
+	}
 
 	/**
 	 * main : Connects to database and prompts users on 4 questions. 
@@ -221,22 +245,31 @@ public class Prog4 {
                                                 sum4 = Integer.parseInt(set4.getString(1));
                                         }
 
-					TreeMap<Integer, String> t = new TreeMap<Integer, String>();
-					System.out.println(sum1);	
+
+                    System.out.println(sum1);	
 					System.out.println(sum2);
 					System.out.println(sum3);
 					System.out.println(sum4);
-					// Put the sums instead of the integers below
-					t.put(sum1, "1");
-					t.put(sum2, "2");
-					t.put(sum3, "3");
-					t.put(sum4, "4");
 					
-					ArrayList<Integer> arr = new ArrayList<Integer>();
-					arr.addAll(t.keySet());
+					Prog4.Pair[] arr = new  Pair[3];
+					arr[0] = new Pair(1, sum1);
+					arr[1] = new Pair(2, sum2);
+					arr[2] = new Pair(3, sum3);
+					arr[3] = new Pair(4, sum4);
+					// Put the sums instead of the integers below
+//					t.put(sum1, "1");
+//					t.put(sum2, "2");
+//					t.put(sum3, "3");
+//					t.put(sum4, "4");
+					
+//					ArrayList<Integer> arr = new ArrayList<Integer>();
+//					arr.addAll(t.keySet());
+					
+					
+					Arrays.sort(arr);
 					
 					for (int i = 0; i < 4; i++) {
-						System.out.println("Dep : " + t.get(arr.get(i)) + " Sum : " + arr.get(i));
+						System.out.println("Dep : " + arr[i].index + " Sum : " + arr[i].value);
 					}
 				} else if (option == 4) {
 					System.out.println("Enter the Department Number");
