4e5de71ceef18ddc43a5ffbc823790e55386c745
Mihir Yadav <Mihiry4@gmail.com>
2021-12-06 09:49:34 -0700

    fixed date but not checked yet by adding todate defaultInsert

--- defaultInserts.java
+++ defaultInserts.java
@@ -9,17 +9,17 @@ public class defaultInserts {
 
 		ArrayList<String> res = new ArrayList<>(
 				List.of(
-						"INSERT INTO customer VALUES (10000,'Amy','apple',1,1000001,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10001,'Bob','branch',1,1000002,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10001,'Bob','branch',2,1000003,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10002,'Camila','carrot',3,1000004,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10003,'Dylan','duck',4,1000005,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10004,'Emily','eagle',1,1000006,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10005,'Fran','fruit',3,1000007,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10006,'Ginny','Grape',1,1000008,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10007,'Hannah','Hat',4,1000009,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10008,'Isabel','Ice',1,NULL,'2021-12-04','2033-12-01');",
-						"INSERT INTO customer VALUES (10009,'Janet','Jar',3,NULL,'2021-12-04','2033-12-01');"));
+						"INSERT INTO customer VALUES (10000,'Amy','apple',1,1000001,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10001,'Bob','branch',1,1000002,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10001,'Bob','branch',2,1000003,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10002,'Camila','carrot',3,1000004,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10003,'Dylan','duck',4,1000005,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10004,'Emily','eagle',1,1000006,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10005,'Fran','fruit',3,1000007,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10006,'Ginny','Grape',1,1000008,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10007,'Hannah','Hat',4,1000009,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10008,'Isabel','Ice',1,NULL,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');",
+						"INSERT INTO customer VALUES (10009,'Janet','Jar',3,NULL,TO_DATE('2021-12-04','YYYY/MM/DD'),TO_DATE('2033-12-01','YYYY/MM/DD');"));
 		return res;
 	}
 
@@ -45,15 +45,15 @@ public class defaultInserts {
 	public ArrayList<String> getDefaultTransactionInserts() {
 		ArrayList<String> res = new ArrayList<>(
 				List.of(
-						"INSERT INTO transact VALUES (1000001,7,1,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000002,7,1,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000003,25,2,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000004,100,3,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000005,12,4,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000006,7,1,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000007,100,3,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000008,7,1,'2021-12-04');",
-						"INSERT INTO transact VALUES (1000009,12,4,'2021-12-04');"));
+						"INSERT INTO transact VALUES (1000001,7,1,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000002,7,1,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000003,25,2,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000004,100,3,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000005,12,4,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000006,7,1,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000007,100,3,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000008,7,1,TO_DATE('2021-12-04','YYYY/MM/DD');",
+						"INSERT INTO transact VALUES (1000009,12,4,TO_DATE('2021-12-04','YYYY/MM/DD');"));
 		return res;
 	}
 
